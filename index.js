a115ca4bed92ff7425ae02cb3244feef6b67537c
Ammar <36680382+smart-programmer@users.noreply.github.com>
2022-07-25 07:19:09 +0000

    convert to express

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1'
-const port = 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
