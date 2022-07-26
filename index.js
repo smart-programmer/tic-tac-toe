ae4da2045958c3a9e2b95ddb894995f539e33f60
Ammar <36680382+smart-programmer@users.noreply.github.com>
2022-07-26 07:22:11 +0000

    hamad initial commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 let port = 5000
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('Hamad is a pussy')
+  res.sendFile("static/html/test.html",{root: path.join(__dirname)},
+              err => {
+                console.log(err)
+              })
 })
 
 
