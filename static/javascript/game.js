75d16f17858f251cb7af670157b02aa125cdf1db
HaMaD <hamad.laaboun@gmail.com>
2022-07-28 12:51:45 +0300

    start scripting the game

---
+++ static/javascript/game.js
@@ -0,0 +1,37 @@
+console.log('hi');
+const borderPosition = document.querySelectorAll('.border-position');
+
+// g.addEventListener('click',()=>{
+//     console.log(this.innerHTML);
+//     console.log('as');
+// })
+var playerTurn = true;
+borderPosition.forEach((item)=> {
+    item.addEventListener('click', ()=> {
+      console.log(item.innerHTML);
+
+
+      if (checkerSpace(item))return;
+
+      playerInput(item);
+
+    });
+     });
+
+     const checkerSpace= (item)=>{
+
+        if (item.innerHTML.charAt(30)!=" "){
+            return true;
+        } 
+        return false;
+     }
+
+     const playerInput = (item)=>{
+        if (playerTurn){ 
+            item.innerHTML=`<span class="input-placement">O</span>`;
+            playerTurn=false; 
+            return;
+          }
+            playerTurn=true;
+            item.innerHTML=`<span class="input-placement">X</span>`;
+     }
