47fb27d747425838a1932b00b2cde64ff3ac71cc
HaMaD <hamad.laaboun@gmail.com>
2022-08-05 04:19:59 +0300

    complete the gamePlay and corrdinating colors

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,43 +1,81 @@
-console.log('hi');
-const borderPosition = document.querySelectorAll('.board');
+const borderPosition = document.querySelectorAll('[data-cell]');
 
-// g.addEventListener('click',()=>{
-//     console.log(this.innerHTML);
-//     console.log('as');
-// })
+const X_CLASS= 'x'
+const O_CLASS= 'o'
+var playerTurn = false;
+
+
 var playerTurn = true;
-borderPosition.forEach((item)=> {
-    item.addEventListener('click', ()=> {
-      // console.log(item.innerHTML);
+borderPosition.forEach(cell=> {
+    cell.addEventListener('click', (e)=> {
+      hoverBorder();
+      swapTurns();
+      whoIsTurn();
+      place(e);
 
-
-      // if (checkerSpace(item))return;
-
-      // playerInput(item);
-
-
-
-      
-
-    });
+      console.log(checkWin());
+      if (checkWin()) {
+        // winner text
+      }
+    },{once:true}); // once mean only you can click one time
      });
 
+     function place(e) {
+      const cell = e.target;
+        cell.classList.add(currentClass)
+       
+     }
+     function whoIsTurn() {
+      if (playerTurn) {
+        currentClass=O_CLASS;
+      } else {
+          currentClass=X_CLASS;
+        }
+        
+     }
+     let currentClass = 'x'
 
 
-    //  const checkerSpace= (item)=>{
 
-    //     if (item.innerHTML.charAt(30)!=" "){
-    //         return true;
-    //     } 
-    //     return false;
-    //  }
 
-    //  const playerInput = (item)=>{
-    //     if (playerTurn){ 
-    //         item.innerHTML=`<span class="input-placement">O</span>`;
-    //         playerTurn=false; 
-    //         return;
-    //       }
-    //         playerTurn=true;
-    //         item.innerHTML=`<span class="input-placement">X</span>`;
-    //  }
+     const board = document.getElementById('board');
+
+     const hoverBorder = ()=>{
+      if (playerTurn) {
+        board.classList.remove('x');
+        board.classList.add('o');
+
+      } else {
+        board.classList.remove('o');
+        board.classList.add('x');
+
+      }
+
+
+     }
+
+
+function swapTurns() {
+  playerTurn=!playerTurn
+
+}
+
+function checkWin () {
+   return winChances.some(c =>{
+    return c.every(index=>{
+      return borderPosition[index].classList.contains(currentClass)
+    })
+  })
+  
+}
+const winChances=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
